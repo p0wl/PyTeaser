e83db75ecc175092309a209e636e971968678d4b
Paul Mölders <paulmoelders@gmail.com>
2013-11-26 00:36:28 +0100

    enables logging and adds example in main def

--- pyteaser.py
+++ pyteaser.py
@@ -5,6 +5,33 @@ from collections import OrderedDict
 stopWords = ["-", " ", ",", ".", "a", "e", "i", "o", "u", "t", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "amoungst", "amount", "an", "and", "another", "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are", "around", "as", "at", "back", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "both", "bottom", "but", "by", "call", "can", "cannot", "can't", "co", "con", "could", "couldn't", "de", "describe", "detail", "did", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fifty", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "got", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed", "into", "is", "it", "its", "it's", "itself", "just", "keep", "last", "latter", "latterly", "least", "less", "like", "ltd", "made", "make", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "new", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own", "part", "people", "per", "perhaps", "please", "put", "rather", "re", "said", "same", "see", "seem", "seemed", "seeming", "seems", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "use", "very", "via", "want", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the", "reuters", "news", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday", "mon", "tue", "wed", "thu", "fri", "sat", "sun", "rappler", "rapplercom", "inquirer", "yahoo", "home", "sports", "1", "10", "2012", "sa", "says", "tweet", "pm", "home", "homepage", "sports", "section", "newsinfo", "stories", "story", "photo", "2013", "na", "ng", "ang", "year", "years", "percent", "ko", "ako", "yung", "yun", "2", "3", "4", "5", "6", "7", "8", "9", "0", "time", "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december", "philippine", "government", "police", "manila"];
 ideal = 20.0
 
+def Summarize2(title, text):
+	summaries = []
+	#try:
+	#	article = grab_link(url)
+	#except IOError:
+	#	print 'IOError'
+	#	return None
+	
+	#print ">>> " + str(high) + " - " + item['Source'] + " >>> " + highsen
+	#if (article is None or article.cleaned_text is None or article.title is None):
+	#	return None
+
+	#text = str(article.cleaned_text.encode('utf-8', 'ignore'))
+	#title = str(article.title.encode('utf-8', 'ignore'))
+	#title = re.sub(r'[^\w ]', '', title) #strip special chars
+	#print title
+	sentences = split_sentences(text)
+	keys =  keywords(text)
+	titleWords = split_words(title)
+
+	#score setences, and use the top 5 sentences
+	ranks = score(sentences, titleWords, keys).most_common(5)
+	for rank in ranks:
+		summaries.append(rank[0])
+
+	return summaries
+
 def Summarize(url):
 	summaries = []
 	try:
@@ -59,6 +86,12 @@ def score(sentences, titleWords, keywords):
 
 		#weighted average of scores from four categories
 		totalScore = (titleFeature*1.5 + frequency*2.0 + sentenceLength*1.0 + sentencePosition*1.0)/4.0
+		
+		print '-------------------------------'
+		print 'SENTENCE: ' + s
+		print 'titleFeature: ' + str(titleFeature) + '	sentenceLength: ' + str(sentenceLength) + '	sentencePositionVar: ' + str(sentencePosition) + '	sbsFeature: ' + str(sbsFeature) + '	dbsFeature: ' + str(dbsFeature) + '	frequency: ' + str(frequency)
+		print 'TOTAL SCORE: ' + str(totalScore)
+
 		ranks[s] = totalScore
 	return ranks
 
@@ -131,10 +164,11 @@ def keywords(text):
 
 def split_sentences(text):
 	#split a large string into sentences
-	import nltk.data #natural language split sentences
-	tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+	#import nltk.data #natural language split sentences
+	#tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 	#text = re.sub(r'[^\w .]', '', text)
-	sentences = tokenizer.tokenize(text)
+	sentences = text.split('.')
+	#sentences = tokenizer.tokenize(text)
 	sentences = [x.replace('\n','') for x in sentences if len(x)>10]
 	return sentences
 
@@ -175,6 +209,11 @@ def sentence_position(i, size):
 		return 0.15
 	else:
 		return 0
+
+
+def main():
+	from pyteaser import Summarize2
+	Summarize2('Framework for Partitioning and Execution of Data Stream Applications in Mobile Cloud Computing', 'The contribution of cloud computing and mobile computing technologies lead to the newly emerging mobile cloud com- puting paradigm. Three major approaches have been pro- posed for mobile cloud applications: 1) extending the access to cloud services to mobile devices; 2) enabling mobile de- vices to work collaboratively as cloud resource providers; 3) augmenting the execution of mobile applications on portable devices using cloud resources. In this paper, we focus on the third approach in supporting mobile data stream applica- tions. More specifically, we study how to optimize the com- putation partitioning of a data stream application between mobile and cloud to achieve maximum speed/throughput in processing the streaming data. To the best of our knowledge, it is the first work to study the partitioning problem for mobile data stream applica- tions, where the optimization is placed on achieving high throughput of processing the streaming data rather than minimizing the makespan of executions as in other appli- cations. We first propose a framework to provide runtime support for the dynamic computation partitioning and exe- cution of the application. Different from existing works, the framework not only allows the dynamic partitioning for a single user but also supports the sharing of computation in- stances among multiple users in the cloud to achieve efficient utilization of the underlying cloud resources. Meanwhile, the framework has better scalability because it is designed on the elastic cloud fabrics. Based on the framework, we design a genetic algorithm for optimal computation parti- tion. Both numerical evaluation and real world experiment have been performed, and the results show that the par- titioned application can achieve at least two times better performance in terms of throughput than the application without partitioning.')
 
 if __name__ == "__main__":
     main()
